a5b2470a9ac46fa640e117f512723f6b9b8bf74d
PriyanshuKumar <priyanshurazz4@gmail.com>
2025-10-19 08:37:33 +0530

    Refactor: Remove existing model files and create new model files with updated structure
    - Deleted old model files for Comment, Like, Playlist, Subscription, Tweet, User, and Video.
    - Created new model files for Comment, Like, Playlist, Subscription, Tweet, User, and Video with updated schemas.
    - Updated routes to reflect changes in controller imports and structure.
    - Added new controller files for Comment, Dashboard, Healthcheck, Like, Playlist, Subscription, Tweet, and User with initial function stubs.
    - Implemented new routes for Comment, Dashboard, Healthcheck, Like, Playlist, Subscription, Tweet, and Video with JWT verification middleware.

--- 03majorProject/src/routes/user.routes.js
+++ 03majorProject/src/routes/user.routes.js
@@ -11,7 +11,7 @@ import {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
-} from "../controllers/user.controllers.js";
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
